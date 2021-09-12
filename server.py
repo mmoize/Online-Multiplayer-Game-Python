01e27147745563b02063ac0505f6a0beadc9d7ac
Moise <murhi46@gmail.com>
2021-09-12 20:03:20 +1000

    Adding Game file, in preparation for Rock, paper scissors game

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #from client import Player
 from os import error
+from player import Player
 import socket
 from _thread import *
 import sys
-
+import pickle
 
 server = "192.168.0.77"
 port = 5555
@@ -21,25 +22,18 @@ s.listen(2)
 print("waiting for a connection, server Started")
 
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
 
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
 
+players = [Player(0, 0, 50, 50, (255, 0, 0, 0)), Player(100, 100, 50, 50, (0, 0, 255))]
 
-pos = [(0,0), (100,100)]
 def threaded_client(conn, player):
-
-    conn.send(str.encode(make_pos(pos[player])))
-    print("the starting pos on server", player)
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048)) 
+            players[player] = data
             
             if not data:
                 print("Disconnected")
@@ -47,14 +41,14 @@ def threaded_client(conn, player):
 
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("Recieved: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
 
         except:
             break
